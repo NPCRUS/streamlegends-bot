0615d61c563e5b4233dc9b9a361fa9ddca992ee9
GrOmAmG <gromamg228@gmai.com>
2020-07-01 14:22:41 +0300

    bot v2

---
+++ test2.js
@@ -0,0 +1,149 @@
+var bot = (() => {
+  const click = new MouseEvent('click', {
+    view: window,
+    bubbles: true,
+    cancelable: true,
+    clientX: 20,
+  })
+
+  let interval = null
+  let mapIndex = 0
+  let areaIndex = 0
+
+  class Stage {
+    constructor() {
+      this.state = Stage.Map
+    }
+
+    set(state) {
+      this.state = state
+    }
+
+    is(state) {
+      this.state === state
+    }
+
+    static get Map() {
+      return 'StreamRpgMap'
+    }
+
+    static get Combat() {
+      return 'StreamRpgCombat'
+    }
+
+    static get Reward() {
+      return 'StreamRpgRewards'
+    }
+
+    static get LeaderBoard() {
+      return 'StreamRpgRaidLeaderboard'
+    }
+
+    static get MapList(){
+      return 'StreamRpgMapList'
+    }
+  }
+
+  return {
+    start: () => setupBotInterval(),
+    stop: () => stop(),
+  }
+
+  function stop(){
+    clearInterval(interval)
+    interval = null
+    mapIndex = 0
+  }
+
+  function setupBotInterval() {
+    if (interval !== null)
+      return console.error('some flow is already running, stop it first')
+
+    mapIndex = getSelectedArea()
+
+    if(mapIndex === null){
+      return console.error('plz start bot only on map')
+    }
+
+    interval = setInterval(() => {
+      let stage = getWindowStage()
+
+      if (stage === Stage.Map) {
+        selectAreaOnMap(mapIndex)
+        pressCombatSectionButton()
+      } else if (stage === Stage.Combat) {
+        postFightButtonFlow()
+      } else if (stage === Stage.Reward) {
+        pressContinueButton()
+      } else if (stage === Stage.LeaderBoard) {
+        pressBackToMap()
+      } else if (stage === Stage.MapList) {
+        console.error('error: jumped to maplist')
+        stop()
+      }
+    }, 1000)
+  }
+
+  function getWindowStage() {
+    return document.querySelector('.srpg-content-scrollbar-scrollable')
+      .lastChild.classList[0]
+  }
+
+  function getSelectedArea() {
+    let mapNodes = document.querySelectorAll('.node-available')
+
+    for (let i = 0; i < mapNodes.length; i++) {
+      const node = mapNodes[i]
+      if (node.classList.contains('node-selected')) return i
+    }
+
+    return null
+  }
+
+  function selectAreaOnMap(index) {
+    let nodes = document.querySelectorAll('.StreamRpgMapNode')
+
+    if (nodes.length === 0) return console.error('cant find mapnodes')
+    if (nodes.length < index)
+      return console.error('cant find available mapnode by index')
+
+    let node = nodes[index]
+
+    if (!node.classList.contains('node-available'))
+      return console.error('finded mapnode cant be selected')
+
+    node.dispatchEvent(click)
+    return true
+  }
+
+  function pressCombatSectionButton() {
+    const fightButton = document.querySelector(
+      '.srpg-map-actions-fight-buttons .btn, .srpg-map-actions-expired-button .btn'
+    )
+    fightButton.dispatchEvent(click)
+    return true
+  }
+
+  function postFightButtonFlow() {
+		const postFightButton = document.querySelector('.post-fight-button')
+    if(postFightButton === null) return false
+    
+    postFightButton.dispatchEvent(click)
+    return true
+  }
+  
+	function pressContinueButton() {
+		const continueButton = document.querySelector('.srpg-button-continue')
+		if(continueButton === null) return false
+
+		continueButton.dispatchEvent(click)
+		return true
+	}
+
+  function pressBackToMap() {
+		const btn = document.querySelector('.srpg-button-maps')
+		if(btn === null) return false
+		btn.dispatchEvent(click)
+		return true
+  }
+})()
